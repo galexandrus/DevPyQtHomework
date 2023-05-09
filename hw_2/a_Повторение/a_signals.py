285b7b9a45e0761552a8faeffea781449ab8b97a
galexandrus <gavrilchuk_a@mail.ru>
2023-05-09 11:17:14 +0300

    repeating a

--- hw_2/a_Повторение/a_signals.py
+++ hw_2/a_Повторение/a_signals.py
@@ -16,6 +16,33 @@ class Window(QtWidgets.QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
 
+        self.initUi()
+        self.initSignals()
+
+    def initUi(self) -> None:
+        self.lineEditInput = QtWidgets.QLineEdit()
+        self.lineEditMirror = QtWidgets.QLineEdit()
+
+        self.pushButtonMirror = QtWidgets.QPushButton()
+        self.pushButtonMirror.setText("Mirror")
+
+        layoutLineEdit = QtWidgets.QHBoxLayout()
+        layoutLineEdit.addWidget(self.lineEditInput)
+        layoutLineEdit.addWidget(self.lineEditMirror)
+
+        layoutMain = QtWidgets.QVBoxLayout()
+        layoutMain.addLayout(layoutLineEdit)
+        layoutMain.addWidget(self.pushButtonMirror)
+
+        self.setLayout(layoutMain)
+
+    def initSignals(self) -> None:
+        self.pushButtonMirror.clicked.connect(self.mirrorText)
+
+    def mirrorText(self) -> None:
+        source_text = self.lineEditInput.text()
+        self.lineEditMirror.setText(source_text[::-1])
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication()
