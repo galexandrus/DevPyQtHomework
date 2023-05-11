a7019c6765a0a278038f0656bb1d31b760fb84e3
galexandrus <gavrilchuk_a@mail.ru>
2023-05-11 07:30:39 +0300

    repeating b, c

--- hw_2/a_Повторение/c_event_filter.py
+++ hw_2/a_Повторение/c_event_filter.py
@@ -7,14 +7,38 @@
 используя html - теги, покрасить разные части текста на нём в разные цвета
 (красивая - красным, кнопка - синим)
 """
+import time
 
-from PySide6 import QtWidgets
+from PySide6 import QtWidgets, QtCore
 
 
 class Window(QtWidgets.QWidget):
 
     def __init__(self, parent=None):
         super().__init__(parent)
+
+        self.initUi()
+
+    def initUi(self) -> None:
+        self.prettyLabel = QtWidgets.QLabel(
+            '<span style="color: red;">Pretty</span> <span style="color:blue;">button</span>'
+        )
+        self.prettyLabel.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+        self.prettyLabel.installEventFilter(self)
+
+        layout = QtWidgets.QHBoxLayout()
+        layout.addWidget(self.prettyLabel)
+
+        self.setLayout(layout)
+
+    def eventFilter(self, watched: QtCore.QObject, event: QtCore.QEvent) -> bool:
+        # print(watched)
+        # print(event)
+
+        if watched == self.prettyLabel and event.type() == QtCore.QEvent.Type.MouseButtonPress:
+            print(time.ctime(), "pretty button pressed")
+
+        return super(Window, self).eventFilter(watched, event)
 
 
 if __name__ == "__main__":
