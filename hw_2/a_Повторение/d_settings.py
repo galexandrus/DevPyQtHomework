ac7870f92cf761ca769ecabdcebffebc593d865a
anjolras <anjolras@yandex.ru>
2023-05-17 12:06:05 +0300

    hw_2 repeating d, c

--- hw_2/a_Повторение/d_settings.py
+++ hw_2/a_Повторение/d_settings.py
@@ -8,13 +8,29 @@
 в него сохранённый текст
 """
 
-from PySide6 import QtWidgets
+from PySide6 import QtWidgets, QtCore, QtGui
 
 
 class Window(QtWidgets.QWidget):
 
     def __init__(self, parent=None):
         super().__init__(parent)
+
+        self.initUi()
+
+    def initUi(self) -> None:
+        settings = QtCore.QSettings("SaveApp")
+        self.plainTextEdit = QtWidgets.QPlainTextEdit()
+        self.plainTextEdit.setPlainText(settings.value("text", ""))
+
+        layout = QtWidgets.QHBoxLayout()
+        layout.addWidget(self.plainTextEdit)
+
+        self.setLayout(layout)
+
+    def closeEvent(self, event: QtGui.QCloseEvent) -> None:
+        settings = QtCore.QSettings("SaveApp")
+        settings.setValue("text", self.plainTextEdit.toPlainText())
 
 
 if __name__ == "__main__":
--- hw_2/a_Повторение/e_custom_signals.py
+++ hw_2/a_Повторение/e_custom_signals.py
@@ -15,13 +15,71 @@
   (пароль можно показать в терминале в захешированном виде)
 """
 
-from PySide6 import QtWidgets
+from PySide6 import QtWidgets, QtCore
 
 
 class Window(QtWidgets.QWidget):
 
     def __init__(self, parent=None):
         super().__init__(parent)
+
+        self.initUi()
+        self.initChildren()
+        self.initSignals()
+
+    def initUi(self):
+        self.labelCompleteRegistration = QtWidgets.QLabel("Complete the registration")
+        self.pushButtonRegistration = QtWidgets.QPushButton("Register")
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self.labelCompleteRegistration)
+        layout.addWidget(self.pushButtonRegistration)
+
+        self.setLayout(layout)
+
+    def initChildren(self):
+        self.registrationDialog = RegistrationDialog()
+
+    def initSignals(self):
+        self.pushButtonRegistration.clicked.connect(self.registrationDialog.exec)
+        self.registrationDialog.registered.connect(self.registerEnded)
+
+    def registerEnded(self, signal_data):
+        self.labelCompleteRegistration.setText(f"Welcome {signal_data[0]}")
+        print("password", signal_data[1])
+
+
+class RegistrationDialog(QtWidgets.QDialog):
+    registered = QtCore.Signal(tuple)
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.initUi()
+        self.initSignals()
+
+    def initUi(self) -> None:
+        self.lineEditLogin = QtWidgets.QLineEdit()
+        self.lineEditLogin.setPlaceholderText("Enter login")
+
+        self.lineEditPassword = QtWidgets.QLineEdit()
+        self.lineEditPassword.setPlaceholderText("Enter password")
+
+        self.pushButtonRegistration = QtWidgets.QPushButton("Registration")
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self.lineEditLogin)
+        layout.addWidget(self.lineEditPassword)
+        layout.addWidget(self.pushButtonRegistration)
+
+        self.setLayout(layout)
+
+    def initSignals(self):
+        self.pushButtonRegistration.clicked.connect(self.onPushButtonRegistrationClicked)
+
+    def onPushButtonRegistrationClicked(self):
+        self.registered.emit((self.lineEditLogin.text(), self.lineEditPassword.text()))
+        self.close()
 
 
 if __name__ == "__main__":
